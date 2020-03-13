56bcb7485fd057e66486a2464f1efa1ce6071458
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-13 19:09:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,7 @@ require 'pry'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-#pp directors_database
+pp directors_database
 
 #Find a way to accumulate the :worldwide_grosses and return that Integer
 #using director_data as input
